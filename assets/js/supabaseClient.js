a43e205fdf1060b9dfad428a55b72544efcbe41f
tommygunn3r <tommygunner@gmail.com>
2025-11-29 14:05:28 -0600

    Update supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // TODO: replace with YOUR values from Supabase project settings
 const SUPABASE_URL = "https://pknhslxhpohrzgsfkisr.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbmhzbHhocG9ocnpnc2ZraXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzQzNjEsImV4cCI6MjA4MDAxMDM2MX0.W3TtsZu0296QKS_k_nNCakh8woCCnERa_Nj_pmVNtwY";
+const SUPABASE_ANON_KEY = "sb_publishable_-zZw-pi_3q1sdNGhITKuhQ_ECyDARzc";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
