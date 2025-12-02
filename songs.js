bd6a068135cb689009c2d790280cc92d21f567ff
tommygunn3r <tommygunner@gmail.com>
2025-12-01 18:33:52 -0600

    Update songs.js

--- songs.js
+++ songs.js
@@ -2,24 +2,18 @@
 // Edit this file to add/remove songs from your jukebox
 
 export const songs = [
-    { title: "Neon Nights", artist: "Synthwave Dreams", file: "music/neon-nights.mp3" },
-    { title: "Arcade Pulse", artist: "Retro Gaming", file: "music/arcade-pulse.mp3" },
-    { title: "Casino Royale", artist: "Jazz Collective", file: "music/casino-royale.mp3" },
-    { title: "Digital Love", artist: "Electric Hearts", file: "music/digital-love.mp3" },
-    { title: "Pixel Paradise", artist: "8-Bit Orchestra", file: "music/pixel-paradise.mp3" },
-    { title: "Gunner's Theme", artist: "GunnersGames", file: "music/gunners-theme.mp3" },
-    { title: "Slots of Fortune", artist: "Vegas Vibes", file: "music/slots-fortune.mp3" },
-    { title: "High Score Hero", artist: "Chiptune Masters", file: "music/high-score.mp3" },
-    { title: "Blackjack Beats", artist: "Card Sharks", file: "music/blackjack-beats.mp3" },
-    { title: "Neon Glow", artist: "Synthwave Dreams", file: "music/neon-glow.mp3" },
-    { title: "Retro Rewind", artist: "80s Revival", file: "music/retro-rewind.mp3" },
+    { title: "1-up Power", artist: "Synthwave Dreams", file: "music/1-Up Power.mp3" },
+    { title: "Block Life", artist: "Retro Gaming", file: "music/Block life.mp3" },
+    { title: "Chopper rap", artist: "Dat' Guy", file: "music/Chopper Rap.mp3" },
+    { title: "Flying cars", artist: "Electric Hearts", file: "music/Flying Cars and Neon Bars.mp3" },
+    { title: "F' Yall", artist: "One Happy Camper", file: "music/Fuck yall.mp3" },
+    { title: "Nidnight Whispers", artist: "Jazz hands", file: "music/Midnight Whispers.mp3" },
+    { title: "Rev it up", artist: "Vegas Vibes", file: "music/Rev It Up.mp3" },
+    { title: "Some BS'", artist: "Master P", file: "music/Some Bullshit.mp3" },
+    { title: "Surfin' engine", artist: "The Cruisers", file: "music/Surfin' Engine.mp3" },
+    { title: "Swept away", artist: "Synthwave Dreams", file: "music/Swept Away by Stardust.mp3" },
+    { title: "The Hand of God", artist: "Cowboy Dude", file: "music/The Hand of God.mp3" },
     { title: "Game Over", artist: "Continue Crew", file: "music/game-over.mp3" },
-    { title: "Winner's Circle", artist: "Victory Lane", file: "music/winners-circle.mp3" },
-    { title: "Midnight Drive", artist: "Outrun Masters", file: "music/midnight-drive.mp3" },
-    { title: "Electric Dreams", artist: "Cyber Punk", file: "music/electric-dreams.mp3" },
-    { title: "Jackpot Journey", artist: "Lucky Strike", file: "music/jackpot-journey.mp3" },
-    { title: "Pixel Power", artist: "Bit Blasters", file: "music/pixel-power.mp3" },
-    { title: "Neon Casino", artist: "Vegas Nights", file: "music/neon-casino.mp3" },
-    { title: "Arcade Anthem", artist: "Joystick Jams", file: "music/arcade-anthem.mp3" },
-    { title: "Cyber Shuffle", artist: "Digital Dealers", file: "music/cyber-shuffle.mp3" }
+    { title: "YO' Dog", artist: "Six7", file: "music/yo' dog.mp3" },
+
 ];
